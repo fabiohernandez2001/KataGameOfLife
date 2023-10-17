cdaca4d7a1dd9191db6cc6a7de0e5260f5d5099e
fabiohernandez2001 <fabiohmaru@gmai.com>
2023-10-17 20:48:30 +0100

    Clases y Lógica básica creada

---
+++ GameOfLife/Board.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Board
+{
+	private Cell[,] cells = new Cell[9, 9]; 
+	public Board()
+	{
+		 for (int i = 0; i < cells.GetLength(0); i++)
+		{
+			for(int j = 0; j < cells.GetLength(1); j++) 
+			{
+				if (i > 3  && j < 9)
+				{
+                    cells[i, j] = new Cell(this, i, j, true);
+                }
+				cells[i, j] = new Cell(this, i, j, false); 
+			
+			}
+		}
+	}
+	public Cell GetCell(int i, int j)
+	{
+		return cells[i, j];
+	}
+	public int[] GetLength()
+	{
+		int[] lengths = new int[2];
+		lengths[0] = cells.GetLength(0);
+		lengths[1] = cells.GetLength(1);
+		return lengths;
+	}
+	public void next()
+	{
+        for (int i = 0; i < cells.GetLength(0); i++)
+        {
+            for (int j = 0; j < cells.GetLength(1); j++) { cells[i, j].check(); }
+        }
+    }
+}
---
+++ GameOfLife/Cell.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+public class Cell
+{
+	private bool isAlive;
+	private int x;
+	private int y;
+	private Board board;
+	public Cell(Board board, int x, int y, bool isAlive)
+	{
+		this.isAlive = isAlive;
+		this.x = x;	
+		this.y = y;
+		this.board = board;
+	}
+	public bool IsAlive { get { return isAlive; } }
+	private void changeState() { isAlive = !isAlive; }
+	private int X { get{ return this.x; } }
+    private int Y { get { return this.y; } }
+    public void check()
+	{
+		int neighborhood = this.neighborhood();
+
+        if (!this.isAlive)
+		{
+			if (neighborhood == 3)
+			{
+				this.changeState();
+			}
+		}
+		else
+		{
+			if (neighborhood <2 || neighborhood>3 ) 
+			{ 
+				this.changeState();
+			}
+		}
+	}
+	private int neighborhood()
+	{
+		int NeighbohrdsAlive = 0;
+		int startX, startY, endX, endY;
+
+        if (this.y == 0) { startY = 0; }
+		else { startY = -1; }
+
+        if (this.x == 0) { startX = 0; }
+		else {  startX = -1; }
+
+        if (this.y == this.board.GetLength()[1])
+        {
+            endY = this.board.GetLength()[1];
+        }
+		else { endY = 2; }
+        
+		if (this.x == this.board.GetLength()[0])
+		{
+			endX = this.board.GetLength()[0];
+            
+        }
+        else { endX = 2; }
+
+        
+        for (int i = startX; i < endX; i++)
+        {
+            for (int j = startY; j < endY; j++)
+            {
+                if (i == 0 && j == 0) continue;
+                if (this.board.GetCell(i + this.x, j + this.x).IsAlive)
+                {
+                    NeighbohrdsAlive++;
+                }
+            }
+        }
+		return NeighbohrdsAlive;
+	}
+}
